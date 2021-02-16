f4f9f24990c439a3cddc0c0d1c8181e4a6ed02fb
camero2734 <42698419+camero2734@users.noreply.github.com>
2021-02-16 17:39:18 -0600

    Update vision_test.py
    Code that is on the Raspberry Pi

--- robot/vision_test.py
+++ robot/vision_test.py
@@ -1,18 +1,67 @@
+vision_test.py
+Not shared
+Type
+Text
+Size
+1 KB (1,030 bytes)
+Storage used
+1 KB (1,030 bytes)
+Location
+Lab 3
+Owner
+me
+Modified
+Feb 11, 2021 by me
+Opened
+5:39 PM by me
+Created
+Feb 11, 2021 with Google Drive Web
+Add a description
+Viewers can download
 import keyboard
 from time import sleep
 from vision import Locator
+# import matplotlib.pyplot as plt
+
 
 locator = Locator()
 
 def wait_keyboard():
     print("Press t to test LiDAR...")
     keyboard.wait("t")
-    print("Grabbing location...")
+    print("\bGrabbing location...")
     
-    locator.locate()
-    locator.print_location()
-
+    try:
+        locator.locate()
+        locator.print_location()
+    except RuntimeError as e:
+        print(e)
+    
     sleep(0.1)
     wait_keyboard()
 
-wait_keyboard()
+def run_tests(num):
+    results_x = []
+    results_y = []
+    results_a = []
+    for i in range(num):
+        try:
+            locator.locate()
+            locator.print_location()
+            results_x.append(locator.x)
+            results_y.append(locator.y)
+            results_a.append(locator.angle)
+        except RuntimeError as e:
+            print(e)
+        sleep(0.1)
+    
+    # t = [(i+1) for i in range(num)]
+    print(results_x)
+    print(results_y)
+    print(results_a)
+    # plt.plot(t, results_x, 'r--', t, results_y, 'bs')
+    #plt.ylabel("Distance (mm)")
+    # plt.show()
+    
+run_tests(1000)
+# wait_keyboard()
