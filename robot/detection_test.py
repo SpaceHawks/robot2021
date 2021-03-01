6c488a8756369b938e0c3d5ecf0c634752401183
Cameron Haley <chaley@hawk.iit.edu>
2021-02-28 18:16:05 -0600

    Detection update to what was changed in lab

--- robot/detection_test.py
+++ robot/detection_test.py
@@ -1,22 +1,22 @@
-import keyboard
+# import keyboard
 from time import sleep
 from vision import Detector
 
 detector = Detector()
 
 def wait_keyboard():
-    print("Press t to test obstacle LiDAR...")
-    keyboard.wait("t")
-    print("\bLooking for obstacles...")
+    print("Running obstacle LiDAR...")
     
-    try:
-        detector.detect(theta=0) # For now, just place the robot at 0 deg so we don't have to test with 2 lidars
-        detector.print_obstacles()
-    except RuntimeError as e:
-        print(e)
+    for i in range(10):
+        print(i)
+        try:
+            detector.detect(theta=0) # For now, just place the robot at 0 deg so we don't have to test with 2 lidars
+            
+        except RuntimeError as e:
+            print(e)
+        
+        sleep(0.5)
     
-    sleep(0.1)
-    wait_keyboard()
-
+    detector.print_obstacles()
     
 wait_keyboard()
